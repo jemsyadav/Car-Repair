6326a8588d2e9ed425b03e262efc8862531d93e1
Jems Yadav <99952368+jemsyadav@users.noreply.github.com>
2023-06-07 18:25:56 +0530

    latest

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -101,7 +101,7 @@ const Services = () => {
       <h4  class="border  ">
       How duis autem in hendrerit in velit molestie consequat ?</h4>
       <div >
-        <p>A 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven’t heard of them accusamus labore sustainable VHS.</p>
+        <p>A assumenda shoreditch et. Nihil anim keff proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven’t heard of them accusamus labore sustainable VHS.</p>
       </div>
       <br />
       <br />
