cb9da83f2dc368c84c24cb8da337176f5184b388
Jems Yadav <99952368+jemsyadav@users.noreply.github.com>
2023-06-07 17:17:05 +0530

    latest

--- src/Pages/Faq.js
+++ src/Pages/Faq.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import vedio from '../Images/vedio.gif'
+
 
 import '../../src/Styles/About.css'
 <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
@@ -7,6 +9,11 @@ const Faq = () => {
   return (
     <div>
    
+  <h1 className='text-center text-red '>COMMING SOON</h1>
+  <h1 className='text-center text-black '>WORK IN PROGRESS</h1>
+  <div className='vedio text-center'>
+  <img  src={vedio} alt=""  width={'90%'}/>
+  </div>
   
     </div>
   )
